bea6d787fab8cf79bd09d1f53fc46f0f30beb075
Olivier Héon <oli@nmd.agency>
2018-06-20 12:38:11 -0400

    Javascript for smooth scrolling

--- source/js/app.js
+++ source/js/app.js
@@ -9,6 +9,7 @@ window.sr = ScrollReveal();
 //     distance: '100px'
 // });
 
+// Javascript for header/nav section
 
 $(document).ready(function(){
   $(window).scroll(function(){
@@ -21,4 +22,30 @@ $(document).ready(function(){
 		  $(".nav-home").css("background" , "none").css("height", "100px");
 	  }
   })
-})
+
+  var scrollLink = $('.scroll');
+
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+
+  // Active link switching
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function() {
+
+      var sectionOffset = $(this.hash).offset().top - 400;
+
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    })
+
+  })
+});
